b7d27dc60c18543c70960677eff8f375d53fc04e
MoonHKLee <mf311110@gmail.com>
2020-11-17 00:51:20 +0900

    update

--- src/main/java/kr/smarket/application/Controller/MemberController.java
+++ src/main/java/kr/smarket/application/Controller/MemberController.java
@@ -46,6 +46,6 @@ public class MemberController {
             Principal principal
     ) {
         memberService.loadUserByUsername(request.getUserId());
-        return "login";
+        return "index";
     }
 }
